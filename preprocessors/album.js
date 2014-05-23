5b268d92dbcc60cc2f387dba74bc9cf3ab4f322e
Rob Gungor <rob@thismoment.com>
2014-05-23 12:42:43 -0500

    adding thumbnails via graph api to homepage, changed out image source for albums gallery and other basic styling

--- preprocessors/album.js
+++ preprocessors/album.js
@@ -3,7 +3,8 @@ module.exports = function( context ){
     for( var i in context.resources.facebook.data ){
 
     	var photoObj = context.resources.facebook.data[i];
-		photoObj.previewImage 	= photoObj.images[0].source;	
+    	// was going to possible do something tricky here, but I think I just like source
+		photoObj.previewImage 	= photoObj.source;//photoObj.images[photoObj.images.length-2].source;	
 		photoObj.humanTime		= new XDate(photoObj.created_time).toString('M/d/yy h(:mm)TT');       
     	
     }
--- preprocessors/albums.js
+++ preprocessors/albums.js
@@ -2,8 +2,9 @@ var XDate = require('xdate');
 module.exports = function( context ){
     for( var i in context.resources.facebook.data ){
     	
-    	var photoObj = context.resources.facebook.data[i];
-		photoObj.humanTime		= new XDate(photoObj.created_time).toString('M/d/yy h(:mm)TT');           	
+    	var albumObj = context.resources.facebook.data[i];
+    	albumObj.previewImage	= "http://graph.facebook.com/"+albumObj.id+"/picture";
+		albumObj.humanTime		= new XDate(albumObj.created_time).toString('M/d/yy h(:mm)TT');           	
 		
     }
 
